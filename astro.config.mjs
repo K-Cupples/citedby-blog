22c952d0218e05d723ef3f4f0e94df56667e5c25
Kyle Cupples <kyle.cupples@codeuntold.com>
2026-03-10 10:26:57 -0700

    Reapply "Migrate blog paths from blog.citedby.co to citedby.co/blog"
    This reverts commit 0690fbb661587c731d9e0095822a7ed7cce949c9.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://blog.citedby.co',
+  site: 'https://citedby.co',
+  base: '/blog',
   integrations: [
     tailwind(),
   ],
